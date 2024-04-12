3d7969ca8f9f6cc71890d0c7b05d557da9d97be3
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2024-04-12 03:42:37 +0000

    Added Car service and api root url to newly created configs.js file

--- src/services/CarService.jsx
+++ src/services/CarService.jsx
@@ -5,10 +5,8 @@ export default class CarService {
 
     static async getVehicles(page, limit, search) {
         try {
-            console.log(`${API_ROOT_URL}/inventory/vehicles?page=${page}&limit=${limit}&search=${search}`);
             const response = await axios
             .get(`${API_ROOT_URL}/inventory/vehicles?page=${page}&limit=${limit}&search=${search}`);
-            console.log(response.data);
             return response.data;
         } catch (error) {
             console.error(error);
