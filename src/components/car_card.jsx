f1b516bbe8cfc03faf435b3760a73d84e7d55236
BakaUSPSB <cjs5@njit.edu>
2024-04-27 19:18:10 -0400

    added modal for car details

--- src/components/car_card.jsx
+++ src/components/car_card.jsx
@@ -1,36 +1,82 @@
-import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Card, Button, Modal } from "react-bootstrap";
 
 const CarCard = ({ vehicle, imageSrc, highlighted }) => {
-
   // if there is a vehicle object, extract the year, make, model, and price otherwise set to empty string
-  vehicle = vehicle || { year: '', make: '', model: '', price: '' };
+  vehicle = vehicle || {
+    year: "",
+    make: "",
+    model: "",
+    price: "",
+    transmission: "",
+    color: "",
+    miles: "",
+    mpg: "",
+    fuel_type: "",
+  };
+  const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const cardStyle = {
-    width: '250px', // Set a fixed width for the card
-    height: '300px', // Set a fixed height for the card
-    border: highlighted ? '2px solid blueviolet' : '2px solid transparent', // Conditionally apply border
-    display: 'flex', // Use flex to control the layout of the card
-    flexDirection: 'column', // Stack the text and button vertically
-    justifyContent: 'space-between' // Distribute space evenly
+    width: "250px", // Set a fixed width for the card
+    height: "300px", // Set a fixed height for the card
+    border: highlighted ? "2px solid blueviolet" : "2px solid transparent", // Conditionally apply border
+    display: "flex", // Use flex to control the layout of the card
+    flexDirection: "column", // Stack the text and button vertically
+    justifyContent: "space-between", // Distribute space evenly
   };
 
   const cardImgStyle = {
-    flex: '1', // Ensure the image takes up available space within the card
-    height: '100%', // Set the height of the image to fill the card
-    width: '100%', // Set the width of the image to fill the card
-    objectFit: 'cover' // Ensure the aspect ratio is maintained
+    flex: "1", // Ensure the image takes up available space within the card
+    height: "100%", // Set the height of the image to fill the card
+    width: "100%", // Set the width of the image to fill the card
+    objectFit: "cover", // Ensure the aspect ratio is maintained
   };
 
   return (
-    <Card style={cardStyle} className="mx-2">
-      <Card.Img variant="top" src={imageSrc} style={cardImgStyle} />
-      <Card.Body>
-        <Card.Text>{vehicle.year + ' ' + vehicle.make + ' ' + vehicle.model}</Card.Text>
-        <Card.Text>{'$' + vehicle.price}</Card.Text>
-        <Button variant="primary">Buy Now</Button>
-      </Card.Body>
-    </Card>
+    <>
+      <Card style={cardStyle} className="mx-2">
+        <Card.Img variant="top" src={imageSrc} style={cardImgStyle} />
+        <Card.Body>
+          <Card.Text>
+            {vehicle.year + " " + vehicle.make + " " + vehicle.model}
+          </Card.Text>
+          <Card.Text>{"$" + vehicle.price}</Card.Text>
+          <Button variant="primary" onClick={handleShow}>
+            Buy Now
+          </Button>
+        </Card.Body>
+      </Card>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ display: "flex", flexDirection: "row" }}>
+          <img
+            src={imageSrc}
+            alt=""
+            style={{ marginRight: "20px", width: "50%" }}
+          />
+          <div>
+            <p>
+              {vehicle.make} {vehicle.model}
+            </p>
+            <p>Color: {vehicle.color}</p>
+            <p>Year:{vehicle.year}</p>
+            <p>${vehicle.price}</p>
+            <p>Transmission: {vehicle.transmission}</p>
+            <p>Miles: {vehicle.miles}</p>
+            <p>MPG: {vehicle.mpg}</p>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
