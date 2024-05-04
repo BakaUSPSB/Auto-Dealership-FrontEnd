d0f412f2a52d57c14531ec71f7d7bd8e5db0d4c7
BakaUSPSB <144304219+BakaUSPSB@users.noreply.github.com>
2024-05-04 19:25:30 -0400

    setup for scheduling and admin dash

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,23 +3,23 @@ import ManagerDash from "../pages/ManagerDash";
 import TechDash from "../pages/TechDash";
 import DashboardPage from "../pages/DashboardPage";
 import LoginPage from "../pages/LoginPage";
+import AdminDash from "../pages/AdminDash";
 
+function Dashboard() {
+  const role = localStorage.getItem("role");
 
-function Dashboard(){
-    const role = localStorage.getItem("role");
-
-
-    switch(role){
-        case "manager":
-            return <ManagerDash />;
-        case "technician":
-            return <TechDash />;
-        case "customer":
-            return <DashboardPage />;
-        default:
-            return <LoginPage />;
-    }
+  switch (role) {
+    case "manager":
+      return <ManagerDash />;
+    case "technician":
+      return <TechDash />;
+    case "customer":
+      return <DashboardPage />;
+    case "admin":
+      return <AdminDash />;
+    default:
+      return <LoginPage />;
+  }
 }
-
 
 export default Dashboard;
