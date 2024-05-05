8edcb0687e38110e1e8cbfbb1a8a606259c6e7b8
KarimSalem9000 <KarimSalem9000@gmail.com>
2024-05-05 19:57:40 -0400

    fixin stuff

---
+++ src/pages/AdminDash.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { Container, Form, Button, Row, Col, Alert } from "react-bootstrap";
+function AdminDash() {
+    // Component logic and JSX here
+  
+    return (
+      <Container>
+        {/* Component JSX */}
+      </Container>
+    );
+  }
+  
+  export default AdminDash;  
